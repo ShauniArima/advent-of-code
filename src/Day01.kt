dc4d40884316ecc45ef5615aa4c180d98c95cb45
Shauni <shaunisoul@protonmail.com>
2023-12-02 00:39:50 +0100

    refactor: Organize code and correct naming

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,57 +1,71 @@
+fun main() {
+
+    fun part1(amendedCalibrationDocument: AmendedCalibrationDocument) =
+        amendedCalibrationDocument
+            .map(AmendedCalibrationValue::removeLetters)
+            .map(AmendedCalibrationValue::toCalibrationValue)
+            .sum()
+
+    fun part2(amendedCalibrationDocument: AmendedCalibrationDocument) =
+        amendedCalibrationDocument
+            .map(AmendedCalibrationValue::convertIntegerNamesToStringWithAnInteger)
+            .map(AmendedCalibrationValue::removeLetters)
+            .map(AmendedCalibrationValue::toCalibrationValue)
+            .sum()
+
+    // test if implementation meets criteria from the description, like:
+    val testInputPart1 = readAmendedCalibrationDocumentFromInput("Day01_test_part1")
+    check(part1(testInputPart1) == 142)
+
+    val testInputPart2 = readAmendedCalibrationDocumentFromInput("Day01_test_part2")
+    check(part2(testInputPart2) == 281)
+
+    val input = readAmendedCalibrationDocumentFromInput("Day01")
+    part1(input).println()
+    part2(input).println()
+}
+
 typealias CalibrationValue = Int
 typealias AmendedCalibrationDocument = List<AmendedCalibrationValue>
 
-@JvmInline
-value class AmendedCalibrationValue(private val value: String) {
-    private fun getFirstDigit() = value.first().digitToInt()
-    private fun getLastDigit() = value.last().digitToInt()
-    fun convertStringsToInt(): AmendedCalibrationValue =
-        AmendedCalibrationValue(
-            value
-                .replace("one", "o1e")
-                .replace("two", "t2o")
-                .replace("three", "t3e")
-                .replace("four", "f4r")
-                .replace("five", "f5e")
-                .replace("six", "s6x")
-                .replace("seven", "s7n")
-                .replace("eight", "e8t")
-                .replace("nine", "n9e")
-        )
-
-    fun removeLetters(): AmendedCalibrationValue =
-        AmendedCalibrationValue(
-            value.replace("[A-Za-z]".toRegex(), "")
-        )
-
-    fun toCalibrationValue(): CalibrationValue = getFirstDigit() * 10 + getLastDigit()
-
-}
-
-fun main() {
-
-    fun readAmendedCalibrationDocument(name: String): AmendedCalibrationDocument = readInput(name)
+fun readAmendedCalibrationDocumentFromInput(name: String): AmendedCalibrationDocument =
+    readInput(name)
         .map(::AmendedCalibrationValue)
 
-    fun part1(amendedCalibrationDocument: AmendedCalibrationDocument) = amendedCalibrationDocument
-        .map(AmendedCalibrationValue::removeLetters)
-        .map(AmendedCalibrationValue::toCalibrationValue)
-        .sum()
+@JvmInline
+value class AmendedCalibrationValue(private val value: String) {
+    private val firstDigit get() = value.first().digitToInt()
+    private val lastDigit get() = value.last().digitToInt()
 
-    fun part2(amendedCalibrationDocument: AmendedCalibrationDocument) = amendedCalibrationDocument
-        .map(AmendedCalibrationValue::convertStringsToInt)
-        .map(AmendedCalibrationValue::removeLetters)
-        .map(AmendedCalibrationValue::toCalibrationValue)
-        .sum()
+    companion object {
+        private val LETTERS_REGEX = "[A-Za-z]".toRegex()
+        private val INTEGER_NAMES_TO_STRING_WITH_AN_INTEGER = mapOf(
+            "one" to "o1e",
+            "two" to "t2o",
+            "three" to "t3e",
+            "four" to "f4r",
+            "five" to "f5e",
+            "six" to "s6x",
+            "seven" to "s7n",
+            "eight" to "e8t",
+            "nine" to "n9e"
+        )
 
-    // test if implementation meets criteria from the description, like:
-    val testInputPart1 = readAmendedCalibrationDocument("Day01_test_part1")
-    check(part1(testInputPart1) == 142)
+        fun convertIntegerNamesToStringWithAnInteger(amendedCalibrationValue: AmendedCalibrationValue): AmendedCalibrationValue =
+            AmendedCalibrationValue(
+                INTEGER_NAMES_TO_STRING_WITH_AN_INTEGER
+                    .entries
+                    .fold(amendedCalibrationValue.value) { value, (integerName, stringWithAnInteger) ->
+                        value.replace(integerName, stringWithAnInteger)
+                    }
+            )
 
-    val testInputPart2 = readAmendedCalibrationDocument("Day01_test_part2")
-    check(part2(testInputPart2) == 281)
+        fun removeLetters(amendedCalibrationValue: AmendedCalibrationValue): AmendedCalibrationValue =
+            AmendedCalibrationValue(
+                amendedCalibrationValue.value.replace(LETTERS_REGEX, "")
+            )
 
-    val input = readAmendedCalibrationDocument("Day01")
-    part1(input).println()
-    part2(input).println()
+        fun toCalibrationValue(amendedCalibrationValue: AmendedCalibrationValue): CalibrationValue =
+            amendedCalibrationValue.firstDigit * 10 + amendedCalibrationValue.lastDigit
+    }
 }
