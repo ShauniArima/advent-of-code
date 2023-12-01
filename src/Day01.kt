caf9e98a1dd54e24d179bfdbf3fb4357cef0c438
Shauni <shaunisoul@protonmail.com>
2023-12-02 00:12:49 +0100

    refactor: Name thing and make it clearer

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,39 +1,57 @@
+typealias CalibrationValue = Int
+typealias AmendedCalibrationDocument = List<AmendedCalibrationValue>
+
+@JvmInline
+value class AmendedCalibrationValue(private val value: String) {
+    private fun getFirstDigit() = value.first().digitToInt()
+    private fun getLastDigit() = value.last().digitToInt()
+    fun convertStringsToInt(): AmendedCalibrationValue =
+        AmendedCalibrationValue(
+            value
+                .replace("one", "o1e")
+                .replace("two", "t2o")
+                .replace("three", "t3e")
+                .replace("four", "f4r")
+                .replace("five", "f5e")
+                .replace("six", "s6x")
+                .replace("seven", "s7n")
+                .replace("eight", "e8t")
+                .replace("nine", "n9e")
+        )
+
+    fun removeLetters(): AmendedCalibrationValue =
+        AmendedCalibrationValue(
+            value.replace("[A-Za-z]".toRegex(), "")
+        )
+
+    fun toCalibrationValue(): CalibrationValue = getFirstDigit() * 10 + getLastDigit()
+
+}
+
 fun main() {
-    fun part1(entries: List<String>): Int {
-        val entriesWithoutLetters = entries
-            .map { it.replace("[A-Za-z]".toRegex(), "") }
 
-        val numberForEachEntries = entriesWithoutLetters
-            .map { it.first().digitToInt() * 10 + it.last().digitToInt() }
+    fun readAmendedCalibrationDocument(name: String): AmendedCalibrationDocument = readInput(name)
+        .map(::AmendedCalibrationValue)
 
-        return numberForEachEntries.sum()
-    }
+    fun part1(amendedCalibrationDocument: AmendedCalibrationDocument) = amendedCalibrationDocument
+        .map(AmendedCalibrationValue::removeLetters)
+        .map(AmendedCalibrationValue::toCalibrationValue)
+        .sum()
 
-    fun part2(entries: List<String>): Int {
-        val entriesWithoutNumberAsStrings = entries
-            .map {
-                it
-                    .replace("one", "o1e")
-                    .replace("two", "t2o")
-                    .replace("three", "t3e")
-                    .replace("four", "f4r")
-                    .replace("five", "f5e")
-                    .replace("six", "s6x")
-                    .replace("seven", "s7n")
-                    .replace("eight", "e8t")
-                    .replace("nine", "n9n")
-            }
-
-        return part1(entriesWithoutNumberAsStrings)
-    }
+    fun part2(amendedCalibrationDocument: AmendedCalibrationDocument) = amendedCalibrationDocument
+        .map(AmendedCalibrationValue::convertStringsToInt)
+        .map(AmendedCalibrationValue::removeLetters)
+        .map(AmendedCalibrationValue::toCalibrationValue)
+        .sum()
 
     // test if implementation meets criteria from the description, like:
-    val testInputPart1 = readInput("Day01_test_part1")
+    val testInputPart1 = readAmendedCalibrationDocument("Day01_test_part1")
     check(part1(testInputPart1) == 142)
-    val testInputPart2 = readInput("Day01_test")
+
+    val testInputPart2 = readAmendedCalibrationDocument("Day01_test_part2")
     check(part2(testInputPart2) == 281)
 
-    val input = readInput("Day01")
+    val input = readAmendedCalibrationDocument("Day01")
     part1(input).println()
     part2(input).println()
 }
